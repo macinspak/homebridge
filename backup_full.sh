983d311c76af21e5f8af613a08aa98b49fcff3a6
macinspak <roo@roo.emu.id.au>
2021-01-08 09:38:34 +1000

    initial

---
+++ backup_full.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd /root/automationbackup
+
+# homebridge
+cp -R /var/lib/homebridge homebridge/var/lib
+cp -R /etc/default/homebridge homebridge/etc/default
+cp -R /etc/systemd/system/homebridge.service homebridge/etc/systemd/system
+
+# system
+cp -R /etc/dhcpd.conf system/etc
+
+# commit any changes to git
+git add -A
+git commit -m "full backup $(date +%F_%R)"
+
+# push
+git push
